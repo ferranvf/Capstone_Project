77649bdd8a3254a58e466e26e51acbe6ea62a9c6
pangacademy <pangacademy@gmail.com>
2023-06-06 15:59:15 -0400

    add some sample files

---
+++ topics.py
@@ -0,0 +1,13 @@
+
+
+class Topics(object):
+
+    def __init__(self, parent, level, name, description):
+
+        self.parent = parent
+        self.level = level
+        self.name = name
+
+        self.description = description
+
+
